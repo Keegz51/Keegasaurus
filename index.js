eceb9078c790d06d5665b2a5646787807716a137
Keegan Naidoo <keegann51@gmail.com>
2020-05-31 14:58:40 +0200

    Removed async
    Removed async causing error

--- index.js
+++ index.js
@@ -11,34 +11,11 @@ bot.on('ready', ()=>{
          console.log('This bot is online!')
      });
 
-bot.on('message', async (msg)=>{
+bot.on('message', (msg)=>{
     let args = msg.content.substring(prefix.length).split(" ");
 
     switch(args[0])
     {
-
-        case 'create':
-            console.log("Got here");
-            const doc= new GuildModel({id: msg.guild.id, demerits: 0});
-            await doc.save;
-            msg.channel.send("Created document");
-
-        break;
-
-        case 'demerit':
-
-            if(args[1].length<=0)
-            {
-                msg.channel.send("Could not execute command, please indicate who you want to demerit");
-            }
-            else
-            {
-                const doc = await GuildModel.findOneAndUpdate({id: msg.guild.id}, {$set: {demerits: (Number)(demerits+5)}},{new: true});
-                return msg.channel.send(doc.id +'s demerits have been updated to  ' +doc.demerits);
-            }
-
-        break;
-
         case 'ping':
             msg.channel.send('pong!');
             break;
@@ -88,14 +65,7 @@ bot.on('message', async (msg)=>{
 
     }
 });
-
-(async()=>{
-    await connect('mongodb://localhost/mongodb-demo', {
-        useNewUrlParser:true,
-        useFindAndModify:false
-    });
-    return bot.login(process.env.token);
-})()
+bot.login(process.env.token);
 
 /* #region Main 
 // bot.on('ready', ()=>{
