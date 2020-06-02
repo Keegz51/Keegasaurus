beb618b3cd210f0ca989c815858d72e6bd095b6d
Keegan Naidoo <keegann51@gmail.com>
2020-06-02 13:37:07 +0200

    Tried adding username back

--- index.js
+++ index.js
@@ -135,6 +135,7 @@ bot.on('message', (msg)=>{
             {
                 embed.addField
                 (
+                    bot.users.fetch(data.user),
                     `${data.user}`, 
                     `${data.points} points (level ${data.level})`
                     
